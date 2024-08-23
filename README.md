5cb56773e0a285a7b2b52a39f3feae58fcc672af
rafael isac <rafael.darrigo@gmail.com>
2024-08-23 04:31:00 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodets-canil
+
+Projeto feito no modulo do curso de node+typescript
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o Projeto
+`npm run start-dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,26 @@
+import { Request,Response } from "express";
+
+export const home=(req:Request,res:Response) =>{
+    //res.render('pages/page');
+    res.send('home no cotroller!');
+
+
+}
+
+export const dogs=(req:Request,res:Response) =>{
+    //res.render('pages/page');
+
+
+}
+
+export const cats=(req:Request,res:Response) =>{
+    //res.render('pages/page');
+
+
+}
+
+export const fishes=(req:Request,res:Response) =>{
+    //res.render('pages/page');
+
+
+}
