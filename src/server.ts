d502ade971a7c8974af4df3a4aaae4d9dc9b4811
rafael isac <rafael.darrigo@gmail.com>
2024-08-29 01:14:24 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ server.use(express.static(path.join(__dirname,'../public')));
 server.use(mainRoutes);
 server.use((req,res)=>{
 
-    res.send('Página nao encontrada!')
+    res.render('pages/404')
 
 })
 
